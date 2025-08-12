2f198a57333c1766fc95b804134f34c77d78959b
Akashkumar888 <akashgkr12@gmail.com>
2025-08-13 00:59:15 +0530

    new folder created

---
+++ Shradha_Mam/first.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Teacher{
+  // access specifier 
+public:
+// properites and variables , attributes
+string name;
+string dept;
+string subject;
+double salary;
+// methods or function 
+
+};
+int main()
+{
+
+return 0;
+}
